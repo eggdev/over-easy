c7e15aa106eda5ce775def8408a14bcec1345437
Brendan Eggers <eggers.brendan@gmail.com>
2023-04-24 16:34:18 -0400

    testing

--- src/views/home/HomeView.js
+++ src/views/home/HomeView.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from './HomeView.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
